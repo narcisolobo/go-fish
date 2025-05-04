94c01de57d74ec98b11bdd94b24055693e2ffe4a
narcisolobo <narcisolobo@gmail.com>
2025-05-04 00:01:02 -0700

    i was in the wrong dir

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,18 +11,17 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends(
-    "next/core-web-vitals",
-    "next/typescript",
-    "plugin:react-hooks/recommended",
-    "plugin:tailwindcss/recommended",
-    "plugin:jest/recommended",
-    "plugin:testing-library/react",
-    "prettier"
+    'next/core-web-vitals',
+    'next/typescript',
+    'plugin:react-hooks/recommended',
+    'plugin:jest/recommended',
+    'plugin:testing-library/react',
+    'prettier'
   ),
   {
     rules: {
-      "react-hooks/exhaustive-deps": "warn",
-      "tailwindcss/no-custom-classname": "off",
+      'react-hooks/exhaustive-deps': 'warn',
+      'tailwindcss/no-custom-classname': 'off',
     },
   },
 ];
